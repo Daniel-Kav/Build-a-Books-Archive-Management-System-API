0ebab3d58b446ad02a09e0c0577affa6127cdead
daniel-kav <dankavatha01@gmail.com>
2025-05-30 11:34:53 +0300

    feat(books): add relations and search functionality to books module
    - Add Author and Category entities to BooksModule imports
    - Change id parameter handling to use UUID
    - Replace Patch with Put for update operation
    - Add search endpoint with title, authorName and categoryName filters
    - Export BooksService and TypeOrmModule for reuse

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, ParseUUIDPipe, Query } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
@@ -18,17 +18,26 @@ export class BooksController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.booksService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.booksService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.booksService.update(+id, updateBookDto);
+  @Put(':id')
+  update(@Param('id', ParseUUIDPipe) id: string, @Body() updateBookDto: UpdateBookDto) {
+    return this.booksService.update(id, updateBookDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.booksService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.booksService.remove(id);
+  }
+
+  @Get('search')
+  searchBooks(
+    @Query('title') title?: string,
+    @Query('authorName') authorName?: string,
+    @Query('categoryName') categoryName?: string,
+  ) {
+    return this.booksService.searchBooks({ title, authorName, categoryName });
   }
 }
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -3,10 +3,13 @@ import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
+import { Author } from '../authors/entities/author.entity';
+import { Category } from '../categories/entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [TypeOrmModule.forFeature([Book, Author, Category])],
   controllers: [BooksController],
   providers: [BooksService],
+  exports: [BooksService, TypeOrmModule],
 })
 export class BooksModule {}
